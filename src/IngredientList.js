ce53bfd0d811b4b61db984ec29c06d31dc16c7bf
chrislee1996 <prepchrislee@gmail.com>
2022-03-24 18:19:19 -0400

    able to get all ingred to render on page

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 
 export default class IngredientList extends Component{
     render() {
+        let allIngredients = this.props.ingredientList.map((ingredient,index)=> {
+            return <li key={index}>{ingredient.name} {ingredient.color}  </li>
+        })
         return (
             <div>
-                <h1>Ingredients</h1>
-                <p>{this.props.ingredientList[0].name} </p>
+                <h1>Ingredients:</h1>
+                <ul>
+                    {allIngredients}
+                </ul>
             </div>
         )
     }
