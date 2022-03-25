f596ce43dabfe167521fc60588a07c0e2815872d
NikPurvis <nikpurvis@gmail.com>
2022-03-24 21:20:50 -0600

    Key name correction

--- src/IngredientsList.js
+++ src/IngredientsList.js
@@ -4,8 +4,8 @@ export default class IngredientsList extends Component {
     
 
     render() {
-        const ingredientOptions = this.props.ingredients.map((ing, i) => {
-        return <li key={i}>{ing.name}</li>
+        const ingredientOptions = this.props.ingredients.map((ingr, i) => {
+        return <li key={i}>{ingr.name}</li>
         })
         return (
             <>
