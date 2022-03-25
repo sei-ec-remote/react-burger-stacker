6f9f050db22cf9ccf21794eca25768acb6046d48
katherine-kania <katherine.kania@gmail.com>
2022-03-24 20:51:29 -0400

    ingredient prop render from Ingredient component

---
+++ src/Ingredient.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Ingredient extends Component {
+    render () {
+        return (
+            <p>{this.props.ingredient}</p>
+        )
+    }
+}
