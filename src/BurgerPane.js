b2e5907132e798ce1e9fa49322d86841887927cc
aoehmann239 <aoehmann239@gmail.com>
2022-03-27 19:26:33 -0400

    Missed a key

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -4,7 +4,13 @@ import Ingredient from './Ingredient';
 export default class BurgerPane extends Component {
   render() {
     let burgerBits = this.props.ingredients.map((ing, i) => (
-      <Ingredient itemKey={i} ingredient={ing} clickFunc={this.props.remove} />
+      <li key={i}>
+        <Ingredient
+          itemKey={i}
+          ingredient={ing}
+          clickFunc={this.props.remove}
+        />
+      </li>
     ));
     return (
       <section className="pane">
