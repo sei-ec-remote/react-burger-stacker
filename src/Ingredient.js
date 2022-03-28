ab393f68f119199eb4f70bfd3faf3da08e18c345
chrislee1996 <prepchrislee@gmail.com>
2022-03-28 19:08:35 -0400

    refractored ingredient

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,19 +1,19 @@
-    import React, { Component } from 'react'
+    import React, { useState } from 'react'
 
 
-export default class Ingredient extends Component {
-    render() {
-        const {name,color} = this.props.ingredient
+const Ingredient = (props) => {
+        const {name,color} = props.ingredient
         // console.log('name of ingred' ,name)
         // console.log('color of ingred' ,color)
         return(
             <p 
                 style={{backgroundColor:color}}
-                id={this.props.itemKey}
-                onClick={this.props.clickFunc}
+                id={props.itemKey}
+                onClick={props.clickFunc}
                 >
                 {name}
             </p>
         )
     }
-}
+
+export default Ingredient
