4809eec112de477b069a93fcbc9bf78d164ada05
kaiguy920 <smithkai920@gmail.com>
2022-03-28 18:44:56 -0400

    BurgerPane converted to funtion component

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -1,32 +1,57 @@
 import React, { Component } from 'react';
 
+const BurgerPane = (props) => {
+    const clickedIngredients = props.burgIngredients.reverse().map((n, i) => {
+        return <li
+            key={i}
+            style={{ backgroundColor: n.color }}
+            onClick={() => props.removeFromStack(n)}
+        > {n.name}</li >
+    })
+    return (
+        <>
+            <div className="column">
+                <h2>Yer Berrgher</h2>
+                <ul
+                    className="items"
+                >
+                    {clickedIngredients}
+                </ul>
+                <button onClick={() => props.clearBurg()}>Clear Burg</button>
+            </div>
+        </>
 
-export default class BurgerPane extends Component {
-
-    render() {
-        // 4. console.log the ingredient list being passed down
-        console.log('ingredient lists', this.props.burgIngredients)
-        const clickedIngredients = this.props.burgIngredients.reverse().map((n, i) => {
-            return <li
-                key={i}
-                style={{ backgroundColor: n.color }}
-                onClick={() => this.props.removeFromStack(n)}
-            > {n.name}</li >
-        })
-
-        return (
-            <>
-                <div className="column">
-                    <h2>Yer Berrgher</h2>
-                    <ul
-                        className="items"
-                    >
-                        {clickedIngredients}
-                    </ul>
-                    <button onClick={() => this.props.clearBurg()}>Clear Burg</button>
-                </div>
-            </>
-
-        )
-    }
+    )
 }
+
+
+// export default class BurgerPane extends Component {
+
+//     render() {
+//         // 4. console.log the ingredient list being passed down
+//         console.log('ingredient lists', this.props.burgIngredients)
+//         const clickedIngredients = this.props.burgIngredients.reverse().map((n, i) => {
+//             return <li
+//                 key={i}
+//                 style={{ backgroundColor: n.color }}
+//                 onClick={() => this.props.removeFromStack(n)}
+//             > {n.name}</li >
+//         })
+
+//         return (
+//             <>
+//                 <div className="column">
+//                     <h2>Yer Berrgher</h2>
+//                     <ul
+//                         className="items"
+//                     >
+//                         {clickedIngredients}
+//                     </ul>
+//                     <button onClick={() => this.props.clearBurg()}>Clear Burg</button>
+//                 </div>
+//             </>
+
+//         )
+//     }
+// }
+export default BurgerPane
