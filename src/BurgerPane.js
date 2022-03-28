a593abc8478cdbdfecc910da63042a0368c686b5
chrislee1996 <prepchrislee@gmail.com>
2022-03-28 19:10:59 -0400

    refractored burgerpane

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Ingredient from './Ingredient'
 
 
-export default class BurgerPane extends Component {
-    render() {
+const BurgerPane = (props) => {
         // console.log('ingred in burger pane',this.props.ingredients)
-        let burgerBits = this.props.ingredients.map((ing,index) => (
+        let burgerBits = props.ingredients.map((ing,index) => (
             <li>
-                <Ingredient itemKey = {index} ingredient={ing} clickFunc={this.props.remove}/>
+                <Ingredient itemKey = {index} ingredient={ing} clickFunc={props.remove}/>
             </li>
         ))
 
@@ -17,8 +16,9 @@ export default class BurgerPane extends Component {
                 <ul>
                     {burgerBits}
                 </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
+                <button onClick={props.clear}>Clear Burger</button>
             </section>
         )
     }
-}
+
+export default BurgerPane
