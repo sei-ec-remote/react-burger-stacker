f6a570fdb7f234e1b12c8729fdaae102794ed0fe
yazeedhani <haniyazeed9@gmail.com>
2022-03-28 19:37:32 -0600

    Converted BurgerPane Component from class to function.

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Ingredient from './Ingredient'
 
 // This is where the burger happens
 // Burger pane needs to display ingredient components, similarly to how IngredientList does
 
-export default class BurgerPane extends Component
+const BurgerPane = (props) =>
 {
     // Burger pane will eventually get props(ingredients), and should loop and display those
     // similar to the IngredientList
     // The difference will be the clear button
-    render()
-    {
         // console.log('ingredients in burger pane', this.props.ingredients)
-        let burgerBits = this.props.ingredients.map( (ing, i) => (
+        let burgerBits = props.ingredients.map( (ing, i) => (
             <li key={i}>
-                <Ingredient itemKey={i} ingredient={ing} clickFunc={this.props.remove} />
+                <Ingredient itemKey={i} ingredient={ing} clickFunc={props.remove} />
             </li>
         ))
         return(
@@ -23,8 +21,9 @@ export default class BurgerPane extends Component
                 <ul>
                     {burgerBits}
                 </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
+                <button onClick={props.clear}>Clear Burger</button>
             </section>
         )
-    }
 }
+
+export default BurgerPane
