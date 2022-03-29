cac60cf58a6911812cfdca035a0651fa601bcc43
yazeedhani <haniyazeed9@gmail.com>
2022-03-28 19:39:28 -0600

    Converted Ingredient Component from class to function.

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 // Ingredient is a hild of Ingredient List
 // It recieves props of an individual ingredient --> color and name
 
-export default class Ingredient extends Component
+const Ingredient = (props) =>
 {
-    render()
-    {   
         // destructuring is how we pull items from an object
         // this looks for properties within the ingredient object
-        const {name, color} = this.props.ingredient
+        const {name, color} = props.ingredient
         // console.log('this is an ingredient', this.props.ingredient.name)
         // console.log('name in each ingredient', name)
         // console.log('color in each ingredient', color)
         return(
             <p 
                 style={{backgroundColor: color}}
-                id={this.props.itemKey}
-                onClick={this.props.clickFunc}
+                id={props.itemKey}
+                onClick={props.clickFunc}
             >
                 {name}
             </p>
         )
-    }
 }
+
+export default Ingredient
