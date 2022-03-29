c7ed869dae66345cf6f3a2279409317cdb862cb4
NikPurvis <nikpurvis@gmail.com>
2022-03-28 21:58:01 -0600

    Refactored BurgerPane with hooks

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -1,34 +1,28 @@
-// This is where the burger happens
-
-import React, { Component } from "react"
-// BurgerPane needs to display ingredeint components, similar to how IngredientsList does.
+import React, { useState } from "react"
 import Ingredient from "./Ingredient"
 
 
-export default class BurgerPane extends Component {
-    // BurgerPane will eventually get props (ingredients0 and should loop and display those props similar to the IngredientList.
-    // The difference will be the clear button (to be added)
-    
-    render() {
-        // console.log("ingr in burgerPane:", this.props.ingredients)
-        let burgerBits = this.props.ingredients.map((ingr, i) => (
-            <li key={i}>
-                < Ingredient
-                    itemKey={i}
-                    ingredient={ingr}
-                    clickFunc={this.props.remove}
-                />
-            </li>
-        ))
+const BurgerPane = (props) => {
 
-        return (
-            <section className="pane">
-                <h3>Burger Pane</h3>
-                <ul>
-                    {burgerBits}
-                </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-        )
-    }
+    let burgerBits = props.ingredients.map((ingr, i) => (
+        <li key={i}>
+            < Ingredient
+                itemKey={i}
+                ingredient={ingr}
+                clickFunc={props.remove}
+            />
+        </li>
+    ))
+
+    return (
+        <section className="pane">
+            <h3>Burger Pane</h3>
+            <ul>
+                {burgerBits}
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+    )
 }
+
+export default BurgerPane
