781b58739148dbabf59a63a313219878169b6f5a
NikPurvis <nikpurvis@gmail.com>
2022-03-28 22:06:14 -0600

    Refactored Ingredient.js with hooks, app refactor complete

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,23 +1,17 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
-// It's just going to display one ingredient.
-// Ingredient is a child of Ingredient List and receives the props of an individual ingredient -> color and name.
+const Ingredient = (props) => {
+    const { name, color } = props.ingredient
 
-export default class Ingredient extends Component {
-    render () {
-        // console.log("props in each ingredient:", this.props.ingredient)
-        const { name, color } = this.props.ingredient   // Deconstruct the object so we can just use the name as in below console.logs
-        // console.log("name in each ingr:", name)
-        // console.log("color in each ingr:", color)
-        return (
-            <p
-                style={{backgroundColor: color}}
-                id={this.props.itemKey}
-                onClick={this.props.clickFunc}
-            >
-                {name}
-            
-            </p>
-        )
-    }
+    return (
+        <p
+            style={{backgroundColor: color}}
+            id={props.itemKey}
+            onClick={props.clickFunc}
+        >
+            {name}
+        </p>
+    )
 }
+
+export default Ingredient
