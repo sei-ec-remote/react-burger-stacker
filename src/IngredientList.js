e0435fbc221c9713f4d5aadd1f2316e53456c1df
katherine-kania <katherine.kania@gmail.com>
2022-03-29 21:21:41 -0400

    IngredientList.js hook convert

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react'
 import Ingredient from './Ingredient'
 
-export default class IngredientList extends Component {
-    render () {
-        // ing= ingredient, i = index from array
-        let allIngredients = this.props.ingredients.map((ing, i) => (
-            <li key={i} >
-                <Ingredient itemKey={i} ingredient={ing} clickFunc={this.props.add}/>
-            </li>
-        ))
-        return (
-            <section className='pane'>
-                <h3>Ingredients List</h3>
-                <ul>
-                    {allIngredients}
-                </ul>
-            </section>
-        )
-    }
+export default function IngredientList (props) {
+  
+    // ing= ingredient, i = index from array
+    let allIngredients = props.ingredients.map((ing, i) => (
+        <li key={i} >
+            <Ingredient itemKey={i} ingredient={ing} clickFunc={props.add}/>
+        </li>
+    ))
+    return (
+        <section className='pane'>
+            <h3>Ingredients List</h3>
+            <ul>
+                {allIngredients}
+            </ul>
+        </section>
+    )
 }
