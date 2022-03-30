4bd0bb7ef97684299f75ae5b78639508fb5a9eda
katherine-kania <katherine.kania@gmail.com>
2022-03-29 21:19:54 -0400

    BurgerPane.js hook convert

--- src/BurgerPane.js
+++ src/BurgerPane.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react'
 import Ingredient from './Ingredient'
 
-export default class BurgerPane extends Component {
-    render () {
-        let burgerBits = this.props.ingredients.map((ing, i) => (
-            <Ingredient 
-                itemKey={i} 
-                ingredient={ing} 
-                clickFunc={this.props.remove} 
-            />
-        ))
-        return (
-            <section className='pane'>
+export default function BurgerPane (props) {
+    
+    let burgerBits = props.ingredients.map((ing, i) => (
+        <Ingredient 
+            itemKey={i} 
+            ingredient={ing} 
+            clickFunc={props.remove} 
+        />
+    ))
+    return (
+        <section className='pane'>
 
-                <h3>Burger Pane</h3>
-                <ul>
-                    {burgerBits}
-                </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-        )
-    }
+            <h3>Burger Pane</h3>
+            <ul>
+                {burgerBits}
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+    )
+    
 }
