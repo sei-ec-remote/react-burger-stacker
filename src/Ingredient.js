0c58c72e22c9f51b04a04cdc56d4213bad2cd5f4
katherine-kania <katherine.kania@gmail.com>
2022-03-29 21:23:23 -0400

    Ingredient.js hook convert

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import './index.css'
 
-export default class Ingredient extends Component {
-    render () {
-        // console.log('props in each ingredient', this.props)
-        const { name, color } = this.props.ingredient
-        // console.log('props in each color', color)
-        // console.log('props in each name', name)
-        return (
-            <p 
-                style={{backgroundColor: color}}
-                id={this.props.itemKey}
-                onClick={this.props.clickFunc}
-            >
-                {name}
-            </p>
-        )
-    }
+export default function Ingredient (props) {
+ 
+    // console.log('props in each ingredient', this.props)
+    const { name, color } = props.ingredient
+    // console.log('props in each color', color)
+    // console.log('props in each name', name)
+    return (
+        <p 
+            style={{backgroundColor: color}}
+            id={props.itemKey}
+            onClick={props.clickFunc}
+        >
+            {name}
+        </p>
+    )
+    
 }
