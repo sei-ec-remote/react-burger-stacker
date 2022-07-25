aee266eb3f8a904bbecfac835b943b1320d5ad7a
cjones1047 <cjmj1047@gmail.com>
2022-07-25 16:05:12 -0500

    Ingredient list shows up with add buttons for each ingredient

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -3,7 +3,7 @@ import IngredientRow from './IngredientRow';
 
 class IngredientList extends Component {
     render() {
-      const allIngredients = this.props.ingredients.name.map((thisIngredient) => {
+      const allIngredients = this.props.ingredients.map((thisIngredient) => {
         return (
             <IngredientRow 
                 ingredient={thisIngredient}
@@ -13,9 +13,9 @@ class IngredientList extends Component {
       return (
         <div className="ingredient-list">
             <h1 className="section-title">Ingredients</h1>
-            <h1>
-                {allIngredients}
-            </h1>
+            <p>
+              {allIngredients}
+            </p>
         </div>
       );
     }
--- src/IngredientRow.js
+++ src/IngredientRow.js
@@ -5,9 +5,11 @@ class IngredientRow extends Component {
       const thisIngredient = this.props.ingredient.name
       return (
         <div className="ingredient-row">
-            <h6>
+            <>
                 {thisIngredient}
-            </h6>
+                {' '}
+            </>
+            <button>+</button>
         </div>
       );
     }
