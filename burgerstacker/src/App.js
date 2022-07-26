a0b65e100cda26730f9a0cb98c916781dba5307a
Pedro Abreu <henriqueibtou@gmail.com>
2022-07-25 18:31:41 -0700

    Added some ingredients and add first btn add/remove ing

---
+++ burgerstacker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Burgerjs from './ingredients/burger';
+
+function App() {
+  return (
+    <div className='bgcontent'>
+      <Burgerjs />
+    </div>
+  );
+}
+
+export default App;
---
+++ burgerstacker/src/ingredients/burger.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import '../ingredients/burgerstyle.css'
+
+export default class Burgerjs extends Component {
+    state = {
+        KaiserBun: 0,
+        SesameBun: 0,
+        GlutenFreeBun: 0,
+        LettuceWrap: 0,
+        BeefPatty: 0,
+        SoyPatty: 0,
+        BlackBeanPatty: 0,
+        ChickenPatty: 0,
+        Lettuce: 0,
+        Tomato: 0,
+        Bacon: 0,
+        Onion: 0
+    }
+
+    render(){
+        return (
+            <>
+                        <div className='bgingredients'>
+                            Burger
+
+                        </div>
+                        <div className='ingredientsblock'>
+                            ingredients
+                            <p>Lettuce</p>
+                            <button onClick={() => this.addRemoveIngredient('add', 'lettuce')} className='ingbtn'>Add</button>
+                            <button className='ingbtn'>Remove</button>
+
+
+                        </div>
+
+            </>
+        );
+    }
+
+
+
+}
+
+
+// [
+//     {name: 'Kaiser Bun', color: 'saddlebrown'},
+//     {name: 'Sesame Bun', color: 'sandybrown'},
+//     {name: 'Gluten Free Bun', color: 'peru'},
+//     {name: 'Lettuce Wrap', color: 'olivedrab'},
+//     {name: 'Beef Patty', color: '#3F250B'},
+//     {name: 'Soy Patty', color: '#3F250B'},
+//     {name: 'Black Bean Patty', color: '#3F250B'},
+//     {name: 'Chicken Patty', color: 'burlywood'},
+//     {name: 'Lettuce', color: 'lawngreen'},
+//     {name: 'Tomato', color: 'tomato'},
+//     {name: 'Bacon', color: 'maroon'},
+//     {name: 'Onion', color: 'lightyellow'}
+//   ]
