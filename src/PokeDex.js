9506f00f4bbb3f3016998090ba6e2ec0596894d4
SunGod7 <sun7ruler@gmail.com>
2022-07-27 08:56:54 -0400

    finished in class

---
+++ src/PokeDex.js
@@ -0,0 +1,19 @@
+
+
+// class PokeDex extends Component {
+//     state = {
+//       pokemon: []
+//     }
+
+// componentDidMount() {
+//     fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+//       .then((res) => res.json())
+//       .then((res) => {
+//         this.setState({pokemon: res.results})
+//     })
+    
+//   }
+  
+
+
+// }
