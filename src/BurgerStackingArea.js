cad7d2de54743894b03ffda7acd8324ae10a938f
Alys <alice.g.cooper@gmail.com>
2022-07-27 18:39:07 -0400

    Updated burgerstackingarea

--- src/BurgerStackingArea.js
+++ src/BurgerStackingArea.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class BurgerStackingArea extends Component {
-    
-    render() {
-        const ingredientItems = this.props.displayIngredients.map((ingredient,idx) => {
-            const color = ingredient.color
-            const ingredientStyle = {
-                backgroundColor: color,
-                width: 400,
-            }
-            return <div key={idx}> <div class="ingredient" style={ingredientStyle} value={ingredient.name}>{ingredient.name}</div></div>
-        })
-        return (
-            <div class="burgerStackerArea">
-                {ingredientItems}
-            </div>
-            )
-    }
+const BurgerStackingArea = (props) => {
+
+    const ingredientItems = props.displayIngredients.map((ingredient,idx) => {
+        const color = ingredient.color
+        const ingredientStyle = {
+            backgroundColor: color,
+            width: 400,
+        }
+        return <div key={idx}> <div class="ingredient" style={ingredientStyle} value={ingredient.name}>{ingredient.name}</div></div>
+    })
+    return (
+        <div class="burgerStackerArea">
+            {ingredientItems}
+        </div>
+        )
+
 }
 
 
