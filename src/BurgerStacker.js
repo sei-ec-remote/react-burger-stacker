2ff8793ba38a568523e1acd1fca3d71e0a077cae
Alys <alice.g.cooper@gmail.com>
2022-07-27 18:30:11 -0400

    ingredient getting adding to burgerIngredients
    but not adding to array

--- src/BurgerStacker.js
+++ src/BurgerStacker.js
@@ -1,82 +1,99 @@
-import React, { Component } from "react";
+import React, { useState } from 'react'
 import IngredientList from './IngredientList'
 import BurgerPane from "./BurgerPane";
 
-class BurgerStacker extends Component {
-	state = {
-		ingredients: [
-			{ name: 'Kaiser Bun', color: 'saddlebrown' },
-			{ name: 'Sesame Bun', color: 'sandybrown' },
-			{ name: 'Gluten Free Bun', color: 'peru' },
-			{ name: 'Lettuce Wrap', color: 'olivedrab' },
-			{ name: 'Beef Patty', color: '#3F250B' },
-			{ name: 'Soy Patty', color: '#3F250B' },
-			{ name: 'Black Bean Patty', color: '#3F250B' },
-			{ name: 'Chicken Patty', color: 'burlywood' },
-			{ name: 'Lettuce', color: 'lawngreen' },
-			{ name: 'Tomato', color: 'tomato' },
-			{ name: 'Bacon', color: 'maroon' },
-			{ name: 'Onion', color: 'lightyellow' },
-		],
-		burgerIngredients: [],
-	}
+const BurgerStacker = () => {
+	// state = {
+	// 	ingredients: [
+	// 		{ name: 'Kaiser Bun', color: 'saddlebrown' },
+	// 		{ name: 'Sesame Bun', color: 'sandybrown' },
+	// 		{ name: 'Gluten Free Bun', color: 'peru' },
+	// 		{ name: 'Lettuce Wrap', color: 'olivedrab' },
+	// 		{ name: 'Beef Patty', color: '#3F250B' },
+	// 		{ name: 'Soy Patty', color: '#3F250B' },
+	// 		{ name: 'Black Bean Patty', color: '#3F250B' },
+	// 		{ name: 'Chicken Patty', color: 'burlywood' },
+	// 		{ name: 'Lettuce', color: 'lawngreen' },
+	// 		{ name: 'Tomato', color: 'tomato' },
+	// 		{ name: 'Bacon', color: 'maroon' },
+	// 		{ name: 'Onion', color: 'lightyellow' },
+	// 	],
+	// 	burgerIngredients: [],
+	// }
+	
+	const ingredients = [
+		{ name: 'Kaiser Bun', color: 'saddlebrown' },
+		{ name: 'Sesame Bun', color: 'sandybrown' },
+		{ name: 'Gluten Free Bun', color: 'peru' },
+		{ name: 'Lettuce Wrap', color: 'olivedrab' },
+		{ name: 'Beef Patty', color: '#3F250B' },
+		{ name: 'Soy Patty', color: '#3F250B' },
+		{ name: 'Black Bean Patty', color: '#3F250B' },
+		{ name: 'Chicken Patty', color: 'burlywood' },
+		{ name: 'Lettuce', color: 'lawngreen' },
+		{ name: 'Tomato', color: 'tomato' },
+		{ name: 'Bacon', color: 'maroon' },
+		{ name: 'Onion', color: 'lightyellow' },
+	]
+
+	// here are newly declared states 
+	const [burgerIngredients, setBurgerIngredients] = useState([])
+	
 
 	// add to the burger
 	// click on an ing and use the `event` to target it
-	addToStack = (e) => {
+	const addToStack = (e) => {
 		// grab the color
 		const ingColor = e.target.style.backgroundColor
 		// grab the name
 		const ingName = e.target.innerText
 		// add to state
-		this.setState({
-			burgerIngredients: [
-				{ name: ingName, color: ingColor },
+		setBurgerIngredients((prevBurgIng) => {
+			const newIng = { name: ingName, color: ingColor }
+			console.log(newIng)
+				//{ name: ingName, color: ingColor },
                 // spread op takes what was in the array and copies it over here
-				...this.state.burgerIngredients,
-			],
+			return [{ ...prevBurgIng, ...newIng}]
+			//],
 		})
 	}
 
 	// remove from burger
-    removeFromStack = (e) => {
+    const removeFromStack = (e) => {
         // select an ing by id
         const clickIndex = e.target.id
         // copy the whole burger
-        const currBurger = this.state.burgerIngredients.slice()
+        const currBurger = burgerIngredients.slice()
         // remove that ing
         currBurger.splice(clickIndex, 1)
         // set that state
-        this.setState({burgerIngredients: currBurger})
+        setBurgerIngredients(currBurger)
     }
 
 	// clear said burger
-    clearBurger = () => {
+    const clearBurger = () => {
         // set state back to an empty array
         // this.setState(() => {
         //     return {
         //         burgerIngredients: []
         //     }
         // })
-        this.setState({ burgerIngredients: [] })
+        setBurgerIngredients( [] )
     }
 
-
-	render() {
 		return (
             <div class="container">
                 <IngredientList 
-                    ingredients={this.state.ingredients} 
-                    add={this.addToStack}
+                    ingredients={ingredients} 
+                    add={addToStack}
                 />
                 <BurgerPane 
-                    ingredients={this.state.burgerIngredients}
-                    remove={this.removeFromStack}
-                    clear={this.clearBurger}
+                    ingredients={burgerIngredients}
+                    remove={removeFromStack}
+                    clear={clearBurger}
                 />
             </div>
         )
-	}
 }
 
 export default BurgerStacker
