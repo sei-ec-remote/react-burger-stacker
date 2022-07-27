27fe51c17c593fb0541f58e83f39a6f2bd9a9be4
Alys <alice.g.cooper@gmail.com>
2022-07-27 18:59:59 -0400

    Set up conditional text formatting in both ingredient list and burger pane

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -7,11 +7,8 @@ const Ingredient = (props) =>  {
     const { name, color } = props.ingredient
     // can we do conditional style formatting so it's easier to see the text?
     // yes - with ternery operator!
-    if (color === 'rgb(63, 37, 11)') {
-        
-    }
     return (
-        <p className={`${color === 'rgb(63, 37, 11)' ? 'whiteText' : ''}`}
+        <p className={`${color === 'rgb(63, 37, 11)' || color === '#3F250B' ? 'whiteText' : ''}`}
             style={{backgroundColor: color}}
             onClick={props.clickFunc}
             id={props.itemKey}
