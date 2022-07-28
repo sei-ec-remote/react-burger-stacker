c7ef2265a962d6ad383c86669f6a55a781a60239
Zene09 <zeneorr@gmail.com>
2022-07-28 07:24:45 -0500

    changed Ingredients to a function

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react'
 import Ingredient from './Ingredients'
 
 const IngredientList = (props) => {
-		// map over the ing
-		// mapped over and created from Ingredient Comp
-        let allIngredients = props.ingredients.map((ing, idx) => (
-            <li key={idx}>
-                <Ingredient 
-                    ingredient={ing}
-                    clickFunc={props.add}
-                    itemKey={idx}
-                />
-            </li>
-        ))
-		return (
-            <>
-            <section>
-                <h3>Ingredients list</h3>
-                <ul>
-                    {allIngredients}
-                </ul>
-            </section>
-            </>
-        )
-	
+    // map over the ing
+    // mapped over and created from Ingredient Comp
+    let allIngredients = props.ingredients.map((ing, idx) => (
+        <li key={idx}>
+            <Ingredient 
+                ingredient={ing}
+                clickFunc={props.add}
+                itemKey={idx}
+            />
+        </li>
+    ))
+    return (
+        <>
+        <section>
+            <h3>Ingredients list</h3>
+            <ul>
+                {allIngredients}
+            </ul>
+        </section>
+        </>
+    )
+
 }
 
 export default IngredientList
