8599cca787e928a4eae36649d00fb6774677665d
Zene09 <zeneorr@gmail.com>
2022-07-28 07:26:25 -0500

    Burger Pane changed to a function

--- src/components/BurgerPane.js
+++ src/components/BurgerPane.js
@@ -2,28 +2,26 @@ import React, { Component } from 'react'
 import Ingredient from './Ingredients'
 
 
-class BurgerPane extends Component {
-    render () {
-        let burgerBits = this.props.ingredients.map((ing, index) => (
-            <li key={index}>
-                <Ingredient 
-                    itemKey={index}
-                    ingredient={ing}
-                    clickFunc={this.props.remove}
-                />
-            </li>
-        ))
-        return (
-            <>
-            <section>
-                <h3> Burger Pane </h3>
-                <ul>
-                    {burgerBits}
-                </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-            </>
-        )
-    }
+const BurgerPane = (props) => {
+    let burgerBits = props.ingredients.map((ing, index) => (
+        <li key={index}>
+            <Ingredient 
+                itemKey={index}
+                ingredient={ing}
+                clickFunc={props.remove}
+            />
+        </li>
+    ))
+    return (
+        <>
+        <section>
+            <h3> Burger Pane </h3>
+            <ul>
+                {burgerBits}
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+        </>
+    )
 }
 export default BurgerPane 
