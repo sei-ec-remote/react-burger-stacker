eded57ba05488cf98a7ce3f36f10546724f4aa8a
nickesparza <nickesparza@gmail.com>
2022-07-28 08:31:03 -0500

    one more tweak to add ingredients to state for possible future functionality

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import IngredientList from './components/IngredientList';
 import BurgerPane from './components/BurgerPane';
 
 
-const ingredients = [
-    {name: 'Kaiser Bun', color: 'saddlebrown'},
-    {name: 'Sesame Bun', color: 'sandybrown'},
-    {name: 'Gluten Free Bun', color: 'peru'},
-    {name: 'Lettuce Wrap', color: 'olivedrab'},
-    {name: 'Beef Patty', color: '#3F250B'},
-    {name: 'Soy Patty', color: '#3F250B'},
-    {name: 'Black Bean Patty', color: '#3F250B'},
-    {name: 'Chicken Patty', color: 'burlywood'},
-    {name: 'Lettuce', color: 'lawngreen'},
-    {name: 'Tomato', color: 'tomato'},
-    {name: 'Bacon', color: 'maroon'},
-    {name: 'Onion', color: 'lightyellow'}
-]
-
 const App = () => {
+    const [ingredients, updateIngredients] = useState(
+        [
+            {name: 'Kaiser Bun', color: 'saddlebrown'},
+            {name: 'Sesame Bun', color: 'sandybrown'},
+            {name: 'Gluten Free Bun', color: 'peru'},
+            {name: 'Lettuce Wrap', color: 'olivedrab'},
+            {name: 'Beef Patty', color: '#3F250B'},
+            {name: 'Soy Patty', color: '#3F250B'},
+            {name: 'Black Bean Patty', color: '#3F250B'},
+            {name: 'Chicken Patty', color: 'burlywood'},
+            {name: 'Lettuce', color: 'lawngreen'},
+            {name: 'Tomato', color: 'tomato'},
+            {name: 'Bacon', color: 'maroon'},
+            {name: 'Onion', color: 'lightyellow'}
+        ]
+    )
     const [chosenIngredients, updateChosenIngredients] = useState([])
 
     const addIngredient = (event) => {
