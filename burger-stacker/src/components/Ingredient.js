30327fa538a32b6f0b6277245613c0acd260d773
erisha <forever.erisha@gmail.com>
2022-07-28 21:02:05 -0400

    converted burger stack components to hooks

--- burger-stacker/src/components/Ingredient.js
+++ burger-stacker/src/components/Ingredient.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-class Ingredient extends Component {
-	render() {
+const Ingredient = (props) => {
+	
 		// I want to render a background color
         // I want to render the name of a ing
         // if there is a key with this `name` extract it
-        const { name, color } = this.props.ingredient
-		return (
-            <p 
-                style={{backgroundColor: color}}
-                onClick={this.props.clickFunc}
-                id={this.props.itemKey}
-            >
-                {name}
-            </p>
-        )
-	}
+    const { name, color } = props.ingredient
+    return (
+        <p 
+            style={{backgroundColor: color}}
+            onClick={props.clickFunc}
+            id={props.itemKey}
+        >
+            {name}
+        </p>
+    )
 }
+
 
 export default Ingredient
--- burger-stacker/src/components/IngredientList.js
+++ burger-stacker/src/components/IngredientList.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Ingredient from './Ingredient'
 
-class IngredientList extends Component {
-	render() {
-		// map over the ing
-		// mapped over and created from Ingredient Comp
-        let allIngredients = this.props.ingredients.map((ing, idx) => (
+const IngredientList = (props) => {
+
+        let allIngredients = props.ingredients.map((ing, idx) => (
             <li key={idx}>
                 <Ingredient 
                     ingredient={ing}
-                    clickFunc={this.props.add}
+                    clickFunc={props.add}
                     itemKey={idx}
                 />
             </li>
@@ -25,6 +23,6 @@ class IngredientList extends Component {
             </>
         )
 	}
-}
+
 
 export default IngredientList
