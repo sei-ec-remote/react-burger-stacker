1739a78424a609cc35af9d17e68f20625daa25cf
f-eiyu <feiyu@alum.mit.edu>
2022-08-09 20:58:14 +0200

    Style the burger half in preparation for functionality

--- src/components/BurgerBubble.js
+++ src/components/BurgerBubble.js
@@ -5,9 +5,15 @@ class BurgerBubble extends Component {
 
   render() {
     return (
-      <>
+      <div id="burger-bubble">
         <h1>Burger</h1>
-      </>
+        <ul id="burger-stack-container">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </div>
     );
   }
 }
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 
 class Ingredient extends Component {
   state = {
-    key: this.props.key,
     name: this.props.name,
     color: this.props.color
   };
 
   render() {
     return (
-      <li key={ this.state.key }>
+      <li>
         <span
           style={{backgroundColor: this.state.color}}
           onClick={ this.props.clickHandler }
