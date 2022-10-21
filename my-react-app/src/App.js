33fd2d4ecdb2fb38347c8a5fdd42337b386a32f7
jlm8965 <JulieLMonsees@gmail.com>
2022-10-21 13:43:04 -0500

    HW complete

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import BurgerPane from './components/BurgerPane'
-import IngredientList from './components/IngredientsList';
-
-const ingredients =
-[
-  {name: 'Kaiser Bun', color: 'saddlebrown'},
-  {name: 'Sesame Bun', color: 'sandybrown'},
-  {name: 'Gluten Free Bun', color: 'peru'},
-  {name: 'Lettuce Wrap', color: 'olivedrab'},
-  {name: 'Beef Patty', color: '#3F250B'},
-  {name: 'Soy Patty', color: '#3F250B'},
-  {name: 'Black Bean Patty', color: '#3F250B'},
-  {name: 'Chicken Patty', color: 'burlywood'},
-  {name: 'Lettuce', color: 'lawngreen'},
-  {name: 'Tomato', color: 'tomato'},
-  {name: 'Bacon', color: 'maroon'},
-  {name: 'Onion', color: 'lightyellow'}
-]
+import './App.css'
+import BurgerStacker from './components/BurgerStacker'
 
 function App() {
   return (
-    //establishing a prop w/ name "ingredients" w values from ingredients array 
-    <div>
-      <BurgerPane ingredients={ingredients} />
+    <div className="App">
+      <BurgerStacker/>
     </div>
   );
 }
--- my-react-app/src/components/BurgerPane.js
+++ my-react-app/src/components/BurgerPane.js
@@ -1,43 +1,28 @@
-import React, {Component} from 'react';
-import BurgerStack from './BurgerStack';
-import ClearBurger from './ClearBurger';
-import IngredientsList from './IngredientsList';
+// this is where we're going to display the burger that we are stacking
+// items clicked in the ingredient list will be added to this component
+import React, { Component } from 'react'
+// since burger pane needs to display ingredient components, we'll import those as well
+import Ingredient from './Ingredient'
 
-
-class BurgerPane extends Component {
-    state = {
-    platedItems: []
-}
-
-// addItem = (item) => {
-//     let newItems = [...this.state.platedItems, item]
-//     console.log("new items here", newItems)
-//     this.setState(
-//         {plateItems: newItems}
-
-//     )
-// }
-render(){
-    console.log(this.props.ingredients)
-     return(
-            <>
-                <IngredientsList ingredients={this.props.ingredients} />
-                    <div className= 'burger-pane'>
-                         Ready for ingredients
-                    <div className='plate'> {this.state.platedItems}</div>
-                </div>
-             </>
+export default class BurgerPane extends Component {
+    render () {
+        let burgerBits = this.props.ingredients.map((ing, i) => (
+            <li key={i}>
+                <Ingredient
+                    itemKey={i}
+                    ingredient={ing}
+                    clickFunc={this.props.remove}
+                />
+            </li>
+        ))
+        return (
+            <section className='pane'>
+                <h3>Burger Pane</h3>
+                <ul>
+                    { burgerBits }
+                </ul>
+                <button onClick={this.props.clear}>Clear Burger</button>
+            </section>
         )
     }
-
 }
-
-export default BurgerPane
-
-
-
-
-
-
-
-
