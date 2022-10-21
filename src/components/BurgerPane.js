b16284b129df697dd682c8d6d999840da1caebc4
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-10-20 22:59:53 -0400

    bare bones

---
+++ src/components/BurgerPane.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Burger extends Component {
+    render() {
+        return (
+            <>
+                <div className='burger'>
+                    This is the BurgerPane
+                </div>
+            </>
+        )
+    }
+}
+
+export default Burger
---
+++ src/components/IngredientList.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import '../index.css'
+
+
+const food = [
+    {name: 'Kaiser Bun', color: 'saddlebrown'},
+    {name: 'Sesame Bun', color: 'sandybrown'},
+    {name: 'Gluten Free Bun', color: 'peru'},
+    {name: 'Lettuce Wrap', color: 'olivedrab'},
+    {name: 'Beef Patty', color: '#3F250B'},
+    {name: 'Soy Patty', color: '#3F250B'},
+    {name: 'Black Bean Patty', color: '#3F250B'},
+    {name: 'Chicken Patty', color: 'burlywood'},
+    {name: 'Lettuce', color: 'lawngreen'},
+    {name: 'Tomato', color: 'tomato'},
+    {name: 'Bacon', color: 'maroon'},
+    {name: 'Onion', color: 'lightyellow'}
+  ]
+
+class IngredientList extends Component {
+    render() {
+        return (
+            <>
+                <div className='ingredients'>
+                    <>{food}</>
+                </div>
+            </>
+        )
+    }
+}
+
+export default IngredientList
