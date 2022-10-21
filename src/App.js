c3ce34b8897dd6693143cccb8aa6a600ee82353f
sheikdon <sheikdonabdiresac@gmail.com>
2022-10-21 12:43:04 -0500

    fixed components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
-import React, { Component } from 'react'
-import './App.css';
-import BurgerPane from './components/BurgerPane';
-import IngredientList from './components/IngredientList';
+import './App.css'
+import BurgerStacker from './BurgerStacker'
 
-
-const ingredientsArr = [
-  { name: 'Kaiser Bun', color: 'saddlebrown' },
-  { name: 'Sesame Bun', color: 'sandybrown' },
-  { name: 'Gluten Free Bun', color: 'peru' },
-  { name: 'Lettuce Wrap', color: 'olivedrab' },
-  { name: 'Beef Patty', color: '#3F250B' },
-  { name: 'Soy Patty', color: '#3F250B' },
-  { name: 'Black Bean Patty', color: '#3F250B' },
-  { name: 'Chicken Patty', color: 'burlywood' },
-  { name: 'Lettuce', color: 'lawngreen' },
-  { name: 'Tomato', color: 'tomato' },
-  { name: 'Bacon', color: 'maroon' },
-  { name: 'Onion', color: 'lightyellow' }
-]
-
-class App extends Component {
-  state = {
-    stackedIngredients: []
-  }
-
-  clearStack = () => {
-    this.setState(() => {
-      return {
-        stackedIngredients: []
-      }
-    })
-  }
-
-  handleClickedIngredient = (event) => {
-    const clickedIngredient = event.target.innerText
-    console.log('app.js', clickedIngredient)
-    this.setState(() => {
-      return {
-        stackedIngredients: [clickedIngredient, ...this.state.stackedIngredients]
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div className='burger-plate'>
-        <BurgerPane ingredients={this.state.stackedIngredients} 
-        clearStack={this.clearStack}/>
-        <IngredientList ingredients={ingredientsArr} 
-        handleClickedIngredient={this.handleClickedIngredient} />
-        
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <BurgerStacker/>
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
