e4617da0bbbfab1f4dcb94951f465ffef5f77c26
mattd01 <matthewdelgado08@gmail.com>
2022-10-21 12:16:25 -0500

    deliverable

---
+++ react-buger-stacker/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Burgerstacker from './components/BurgerStacker';
+
+function App() {
+  return (
+    <div className='app'>
+      <Burgerstacker />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-buger-stacker/src/components/ClearBurger.js
@@ -0,0 +1,12 @@
+// import { Component } from "react";
+
+
+// class ClearBurger extends Component {
+//     render() {
+//         <div>
+//             <button onClick={}></button>
+//         </div>
+//     }
+// }
+
+// export default ClearBurger
