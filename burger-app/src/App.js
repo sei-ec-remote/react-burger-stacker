7f72d636f462f3d1a051678125d8d806d086f891
Salam337 <salamadash@gmail.com>
2022-10-23 20:06:52 -0400

    burger stack app

---
+++ burger-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import BurgerStacker from './BurgerStacker'
+
+function App() {
+  return (
+    <div className="App">
+      <BurgerStacker/>
+    </div>
+  );
+}
+
+export default App;
