905ba3040c8af8d786fa2225aafdb06bf9facd64
megwelch <meghanmwelch@gmail.com>
2022-10-24 18:51:01 -0400

    changed class components to functions

--- src/components/BurgerApp.js
+++ src/components/BurgerApp.js
@@ -1,52 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import IngredientList from './IngredientList'
 
-class BurgerApp extends Component {
-    state = {
-        platedItems: []
-    }
+const BurgerApp = (props) => {
+    const [platedItems, setPlatedItems] = useState([])
+    // state = {
+    //     platedItems: []
+    // }
 
-    addItem = (e) => {
+    const addItem = (e) => {
         console.log('click', e.target)
-        this.setState(
-           (prevState => ({
-            platedItems: [...prevState.platedItems, e.target.textContent]
-           })) 
-        )
+        setPlatedItems( prevPlatedItems => {
+            return [...prevPlatedItems, e.target.textContent]
+           })
     }
 
-    clearBurger = () => {
+    const clearBurger = () => {
         console.log('click')
-        this.setState({
-            platedItems: []
-        })
+        setPlatedItems([])
     }
 
-    render(){
+    const burgerItems = platedItems.map((item) => {
+        console.log(item, 'this is burger item')
+        return <div>{item}</div>
+    })
 
-
-        const burgerItems = this.state.platedItems.map((item) => {
-            console.log(item, 'this is burger item')
-            return <div>{item}</div>
-        })
-        return(
-            <>
-            <h1>Build a Burger</h1>
-            <div className="burger-container">
-                <div className="ingredients">
-                    <IngredientList ingredients={this.props.ingredients} addItem={this.addItem}/>
-                </div>
-                <div className="plate">
-                    {burgerItems}
-                </div>
-                <button className="burger-btn" onClick={this.clearBurger}>Clear<br></br>Burger</button>
+    return(
+        <>
+        <h1>Build a Burger</h1>
+        <div className="burger-container">
+            <div className="ingredients">
+                <IngredientList ingredients={props.ingredients} addItem={addItem}/>
             </div>
-            
-            </>
-        )
-    }
+            <div className="plate">
+                {burgerItems}
+            </div>
+            <button className="burger-btn" onClick={clearBurger}>Clear<br></br>Burger</button>
+        </div>
+        </>
+    )
 }
 
 export default BurgerApp
-
-// onClick={(e) => this.props.addItem(e.target.value)}
--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,16 +1,32 @@
-import React, { Component } from 'react' 
+import React from 'react' 
 
-class IngredientList extends Component {
+const IngredientList = (props) => {
 
-    render () {
-        const ingredientItems = this.props.ingredients.map(ingredient => 
-            <li><button onClick={(e) => this.props.addItem(e)} key={ingredient}>{ingredient.name}</button></li>)
+        const ingredientItems = props.ingredients.map(ingredient => 
+            <li><button onClick={(e) => props.addItem(e)} key={ingredient}>{ingredient.name}</button></li>)
         return (
             <>
                 <ul>{ingredientItems}</ul>
             </> 
         )
-    }
+
 }
 
 export default IngredientList
+
+// import React, { Component } from 'react' 
+
+// class IngredientList extends Component {
+
+//     render () {
+//         const ingredientItems = this.props.ingredients.map(ingredient => 
+//             <li><button onClick={(e) => this.props.addItem(e)} key={ingredient}>{ingredient.name}</button></li>)
+//         return (
+//             <>
+//                 <ul>{ingredientItems}</ul>
+//             </> 
+//         )
+//     }
+// }
+
+// export default IngredientList
