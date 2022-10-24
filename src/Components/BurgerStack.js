367fb54597fa5917636bd3332de6d55cadf57afd
lozier <latayroutier@gmail.com>
2022-10-24 17:45:38 -0400

    converted BurgerStack to function component

--- src/Components/BurgerStack.js
+++ src/Components/BurgerStack.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import IngredientList from './IngredientList'
 
-class BurgerStack extends Component {
-    render() {
-        let burgerParts = this.props.ingredients.map(ingredient => 
-            <li>
-                <IngredientList ingredient={ingredient} />
-            </li>)
+const BurgerStack = (props) => {
+    
+    let burgerParts = props.ingredients.map(ingredient => 
+        <li>
+            <IngredientList ingredient={ingredient} />
+        </li>)
 
-        return (
-            <div>
-                <h2>BURGER STACK</h2>
-                <ul className='burger'>
-                    {burgerParts}
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>BURGER STACK</h2>
+            <ul className='burger'>
+                {burgerParts}
+            </ul>
+        </div>
+    )
 }
 
 export default BurgerStack
