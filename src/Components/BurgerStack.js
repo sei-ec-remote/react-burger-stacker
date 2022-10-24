67a87d1f76518536c37d7a42969b0546fa0f0c6c
OnePartHarmony <bakingharmony@gmail.com>
2022-10-24 17:22:11 -0400

    BurgerStack now function component

--- src/Components/BurgerStack.js
+++ src/Components/BurgerStack.js
@@ -1,19 +1,19 @@
 import React from "react"
 import Ingredient from "./Ingredient"
 
-class BurgerStack extends React.Component {
-    render () {
-        const stack = this.props.stack
-        let stackItems = stack.map((item, index) => {
-            return <Ingredient key={index} index={index} name={item.name} color={item.color} textColor={item.textColor} clickItem={this.props.removeItem} /> 
-        })
-        
-        return (
-                <div id="Stack">
-                    {stackItems}
-                </div>
-        )
-    }
+const BurgerStack = (props) => {
+    
+    const stack = props.stack
+    let stackItems = stack.map((item, index) => {
+        return <Ingredient key={index} index={index} name={item.name} color={item.color} textColor={item.textColor} clickItem={props.removeItem} /> 
+    })
+    
+    return (
+            <div id="Stack">
+                {stackItems}
+            </div>
+    )
+
 }
 
 export default BurgerStack
