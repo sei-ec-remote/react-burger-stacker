9a36cac51af5af043490fc9091eb089571f32180
lozier <latayroutier@gmail.com>
2022-10-24 17:43:19 -0400

    converted BurgerPane to function component

--- src/Components/BurgerPane.js
+++ src/Components/BurgerPane.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Ingredient from './Ingredient'
 
-class BurgerPane extends Component {
-    render() {
-        let burgerParts = this.props.ingredients.map((ing, i) => (
-            <li key={i}>
-                <Ingredient
-                    itemkey={i}
-                    ingredient={ing}
-                    clickFunc={this.props.remove}
-                />
-            </li>
-        ))
-        
-        return (
-            <section className='pane'>
-                <h1>BURGER PANE</h1>
-                <ul>
-                    {burgerParts}
-                </ul>
-               <button onClick={this.props.clear}>Clear Burger</button> 
-               
-                    
-            </section>
-        )
-    }
+const BurgerPane = (props) => {
+    
+    let burgerParts = props.ingredients.map((ing, i) => (
+        <li key={i}>
+            <Ingredient
+                itemkey={i}
+                ingredient={ing}
+                clickFunc={props.remove}
+            />
+        </li>
+    ))
+
+    return (
+        <section className='pane'>
+            <h1>BURGER PANE</h1>
+            <ul>
+                {burgerParts}
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button> 
+            
+                
+        </section>
+    )
+    
 }
 
 export default BurgerPane
