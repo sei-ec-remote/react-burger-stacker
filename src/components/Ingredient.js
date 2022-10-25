833811a3c124f846c60178309a94904ff097b3ad
nabbott98 <nicholasabbott98@gmail.com>
2022-10-24 20:36:16 -0400

    Ingredient Converted to hooks

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -2,18 +2,18 @@
 import React, { Component } from 'react'
 
 // this component recieves ONE ingredient as a prop and displays said ingredient
-export default class Ingredient extends Component {
-    render () {
-        const { name, color } = this.props.ingredient
+const Ingredient = () => {
+    const { name, color } = this.props.ingredient
 
-        return (
-            <p
-                style={{backgroundColor: color}}
-                id={this.props.itemKey}
-                onClick={this.props.clickFunc}
-            >
-                {name}
-            </p>
-        )
-    }
+    return (
+        <p
+            style={{backgroundColor: color}}
+            id={this.props.itemKey}
+            onClick={this.props.clickFunc}
+        >
+            {name}
+        </p>
+    )
 }
+
+export default Ingredient
