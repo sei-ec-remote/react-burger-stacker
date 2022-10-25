2c3aebc97ffa3c97491d1e6defc204322f0da9df
zyaffee <zyaffee@gmail.com>
2022-10-24 21:37:57 -0400

    Ingredient List component now a functional function

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react"
 import Ingredient from "./Ingredient"
 
-export default class IngredientList extends Component {
-    render () {
-        let allIngredients = this.props.ingredients.map((ing, i) => (
-            <li key={i}>
-                <Ingredient
-                    itemKey = {i}
-                    ingredient={ing}
-                    clickFunc={this.props.add}
-                />
-            </li>
-        ))
+const IngredientList = (props) => {
 
-        return (
-            <section className="pane">
-                <h3>Ingredients List</h3>
-                <ul>
-                    {allIngredients}
-                </ul>
-            </section>
-        )
-    }
+    let allIngredients = props.ingredients.map((ing, i) => (
+        <li key={i}>
+            <Ingredient
+                itemKey={i}
+                ingredient={ing}
+                clickFunc={props.add}
+            />
+        </li>
+    ))
+
+    return (
+        <section className="pane">
+            <h3>Ingredients List</h3>
+            <ul>
+                {allIngredients}
+            </ul>
+        </section>
+    )
 }
+
+export default IngredientList
