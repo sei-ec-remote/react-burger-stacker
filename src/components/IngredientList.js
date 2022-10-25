bd6785cf866f8ca2d143379daa571f30bc7d6894
nabbott98 <nicholasabbott98@gmail.com>
2022-10-24 20:34:52 -0400

    IngredientList Converted to hooks

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,25 +1,26 @@
 // loops over ingredients from props and displays one Ingredient component per item
 import React, { Component } from 'react'
 import Ingredient from './Ingredient'
+import { useState } from 'react'
 
-export default class IngredientList extends Component {
-    render () {
-        let allIngredients = this.props.ingredients.map((ing, i) => (
-            <li key={i}>
-                <Ingredient
-                    itemKey={i}
-                    ingredient={ing}
-                    clickFunc={this.props.add}
-                />
-            </li>
-        ))
-        return (
-            <section className='pane'>
-                <h3>Ingredients List</h3>
-                <ul>
-                    {allIngredients}
-                </ul>
-            </section>
-        )
-    }
+ const IngredientList = () => {
+    let allIngredients = this.props.ingredients.map((ing, i) => (
+        <li key={i}>
+            <Ingredient
+                itemKey={i}
+                ingredient={ing}
+                clickFunc={this.props.add}
+            />
+        </li>
+    ))
+    return (
+        <section className='pane'>
+            <h3>Ingredients List</h3>
+            <ul>
+                {allIngredients}
+            </ul>
+        </section>
+    )
 }
+
+export default IngredientList
