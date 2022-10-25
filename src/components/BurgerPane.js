28134e98c0ac0adf65a16fd6d55d0a1ef213d59a
zyaffee <zyaffee@gmail.com>
2022-10-24 21:43:34 -0400

    Ingredient now function

--- src/components/BurgerPane.js
+++ src/components/BurgerPane.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react"
 import Ingredient from "./Ingredient"
 
-export default class BurgerPane extends Component {
-    render() {
-        let burgerIngs = this.props.ingredients.map((ing, i) => (
-            <li key={i}>
-                <Ingredient
-                    itemKey={i}
-                    ingredient={ing}
-                    clickFunc={this.props.remove}
-                />
-            </li>
-        ))
+const BurgerPane = (props) => {
+    let burgerIngs = props.ingredients.map((ing, i) => (
+        <li key={i}>
+            <Ingredient
+                itemKey={i}
+                ingredient={ing}
+                clickFunc={props.remove}
+            />
+        </li>
+    ))
 
-        return (
-            <section className="pane">
-                <h3>Burger Pane</h3>
-                <section className="bottomUp">
-                    <ul className="bottoms">
-                        {burgerIngs}
-                    </ul>
-                    <button className="bottoms" onClick={this.props.clear}>Clear Burger</button>
-                </section>
+    return (
+        <section className="pane">
+            <h3>Burger Pane</h3>
+            <section className="bottomUp">
+                <ul className="bottoms">
+                    {burgerIngs}
+                </ul>
+                <button className="bottoms" onClick={props.clear}>Clear Burger</button>
             </section>
-        )
-    }
+        </section>
+    )
 }
+
+export default BurgerPane
