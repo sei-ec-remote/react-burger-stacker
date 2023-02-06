f7f3d9a99071c5644f1601d14b9b73af20cfeaef
AlexMcBex <alessiobelli94@gmail.com>
2023-02-06 17:33:20 -0500

    finished deliverable

--- src/components/BurgerPane.jsx
+++ src/components/BurgerPane.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import Ingredient from "./ingredient";
 
-export default class BurgerPane extends Component{
+class BurgerPane extends Component{
     render(){
-         let burgerBits = this.props.ingredients.map((ing, i)=> (
-        <li key={i}>
-            <Ingredient 
-            itemKey={i}
-            ingredient={ing}
-            clickFunc={this.props.remove}
-            />
-        </li>
+         let burgerIngredients = this.props.ingredients.map(ing=> (
+            <li >
+            <p className="burger" style={{backgroundColor: ing.color}}> 
+                </p>
+          </li>
     ))
             return(
-                <section className="pane">
+                <section className="column">
                     <h3>Burger Pane</h3>
                     <ul>
-                        { burgerBits }
+                        { burgerIngredients }
                     </ul>
-                    <button onClick={this.props.clear}>Clear Burger</button>
+                    <button onClick={this.props.clear}>Clear </button>
                 </section>
             )
 }
 }
+export default BurgerPane
--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,26 +1,24 @@
 import React, { Component } from "react"
-import Ingredient from "./ingredient"
 
-export default class IngredientList extends Component {
+class IngredientList extends Component {
   render() {
-    let allIngredients = this.props.allIngredients.map((ing, i) => (
-      <li key={i}>
-        <Ingredient
-          ingredient={ing}
-          itemKey={i}
-          clickFunc={this.props.add}
-        />
+    let Ingredients = this.props.Ingredients.map(ing => (
+      <li >
+        <p className="ingredient" style={{backgroundColor: ing.color}} onClick={this.props.add}> 
+            {ing.name} 
+            </p>
       </li>
     ))
     return (
-      <section className="pane">
+      <section className="column">
 
         <h3> Ingredient List</h3>
         <ul>
-          {allIngredients}
+          {Ingredients}
         </ul>
       </section>
     )
 
   }
 }
+export default IngredientList
