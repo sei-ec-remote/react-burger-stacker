6c5e693af610de022f881725f7e85beb04ae857e
jawkneewin <johnnytnguyen93@gmail.com>
2023-02-06 20:34:29 -0600

    Finished class component burger stacker deliverable

---
+++ src/Ingredient.js
@@ -0,0 +1,20 @@
+// this displays individual ingredients
+import React, { Component } from "react"
+
+// recieves one ingredient as a prop, and displays said ingredient
+export default class Ingredient extends Component {
+    render () {
+        // destructure our ingredients properties
+        const { name, color } = this.props.ingredient
+
+        return (
+            <p 
+                style={{backgroundColor: color}}
+                id={this.props.itemKey}
+                onClick={this.props.clickFunc}
+            >
+                {name}
+            </p>
+        )
+    }
+}
