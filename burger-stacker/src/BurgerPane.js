8f48a73bfea7ceb04023d69aeb4e46983b8766d5
KenGibb <kenjibbo@gmail.com>
2023-02-07 19:07:42 -0600

    complete states

--- burger-stacker/src/BurgerPane.js
+++ burger-stacker/src/BurgerPane.js
@@ -1,30 +1,30 @@
 // this will loop over an array of ingredients, that were added by clicking each ingredient in the ingredientList
 
-import React, { Component } from "react"
+// import React, { Component } from "react"
 // since Ingredient is a reusable component, we can import that too
 import Ingredient from "./Ingredient"
 
-export default class BurgerPane extends Component {
-    render () {
-        let burgerBits = this.props.ingredients.map((ing, i) => (
-            <li key={i}>
-                <Ingredient 
-                    itemKey={i}
-                    ingredient={ing}
-                    clickFunc={this.props.remove}
-                />
-            </li>
-        ))
+const BurgerPane = (props) => {
+    let burgerBits = props.ingredients.map((ing, i) => (
+        <li key={i}>
+            <Ingredient 
+                itemKey={i}
+                ingredient={ing}
+                clickFunc={props.remove}
+            />
+        </li>
+    ))
 
-        return (
-            <section className='pane'>
-                <h3>Burger Pane</h3>
-                <ul>
-                    { burgerBits }
-                </ul>
-                <button onClick={this.props.clear}>Clear Burger</button>
-            </section>
-        )
-    }
+    return (
+        <section className='pane'>
+            <h3>Burger Pane</h3>
+            <ul>
+                { burgerBits }
+            </ul>
+            <button onClick={props.clear}>Clear Burger</button>
+        </section>
+    )
 
 }
+
+export default BurgerPane
