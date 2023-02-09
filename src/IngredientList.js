48552d78afaf479894b1d428678e4693e8136f20
devrajn <devraj.nepal@gmail.com>
2023-02-08 22:06:30 -0600

    another Class based burger-stacker commit

---
+++ src/IngredientList.js
@@ -0,0 +1,30 @@
+// loops over the ingredients derived from props
+// produces a clickable ingredient for each of them
+
+import React, { Component } from 'react'
+import Ingredient from './Ingredient'
+
+ class IngredientList extends Component {
+    render () {
+        let allIngredients = this.props.ingredients.map((ing, i) => (
+            // this produces list items
+            <li key={i}>
+                <Ingredient
+                    ingredient={ing}
+                    itemKey={i}
+                    clickFunc={this.props.add}
+                />
+            </li>
+        ))
+
+        return (
+            <section className='pane'>
+                <h3>Ingredients List</h3>
+                <ul>
+                    { allIngredients }
+                </ul>
+            </section>
+        )
+    }
+}
+export default IngredientList
