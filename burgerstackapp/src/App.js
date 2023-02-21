ed44a1f72de066b181cb8da61da1d9aca6d95b2a
nocnoc_e <dhgustmd25@hotmail.com>
2023-02-20 20:55:34 -0500

    adding and clearing button works

---
+++ burgerstackapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import BurgerStacker from './BurgerStacker'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BurgerStacker />
+    </div>
+  );
+}
+
+export default App;
